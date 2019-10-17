be251acca25674fca61ee0bdda5efca77053f51b
Samuel Dunn <samuel.i.dunn@wsu.edu>
2019-10-17 12:50:48 -0700

    Include project meta files
    requirements.txt specifies modules we'll need for development.
    setup.py specifies how the project's wheel should be bundled.

---
+++ setup.py
@@ -0,0 +1,30 @@
+# Samuel Dunn
+# CS320, POFIS.
+# This setup script manages bundling POFIS into a redistributable wheel.
+# (wheel is the python package format)
+
+from setuptools import setup, find_packages
+
+# POFIS's version (may want to import this from POFIS
+# directly in the future.)
+version = '0.0.0b1'
+
+# Dependencies for POFIS runtime.
+dependencies = ('cherrypy', )
+
+# Any non-python files to include within the distribution
+# (Such as templates, html files, .rst files, etc.)
+static_data = {
+
+}
+
+if __name__ == "__main__":
+    setup(
+        name="pofis",
+        version=version,
+        packages=find_packages(),
+        package_data=static_data,
+        author="Abdi Vicenciodelmoral, Andrew Cornish, Becca Daniels, Samuel Dunn",
+        # include emails in the future.
+        install_requires=dependencies
+    )
