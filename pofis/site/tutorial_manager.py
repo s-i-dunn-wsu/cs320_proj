e980a25b8606e39ec65e1ea6cde1ab122f130c4a
Samuel Dunn <samuel.i.dunn@wsu.edu>
2019-12-10 17:35:13 -0800

    Squashed commit of the following:
    commit 3534e48a7b9976507d625f08137bb6299e606813
    Author: Samuel Dunn <samuel.i.dunn@wsu.edu>
    Date:   Tue Dec 10 17:35:02 2019 -0800
    
        complete the mass rename. (I think I got everything... )
    
    commit e89b51e73a58ca65b55350b6c7c81a8ce0990ca7
    Author: Samuel Dunn <samuel.i.dunn@wsu.edu>
    Date:   Tue Dec 10 17:12:22 2019 -0800
    
        Do a mass rename, contents not yet adjusted.

--- pofis/site/tutorial_manager.py
+++ pofis/site/tutorial_manager.py
@@ -1,11 +1,11 @@
 
 import cherrypy
 
-from ..test_suite.progress_tracker import ProgressTracker
-from ..test_suite.tsf import TestSuiteFactory
+from ..tutorial.progress_tracker import ProgressTracker
+from ..tutorial.tutorial_factory import TutorialFactory
 
 @cherrypy.expose
-class SuiteManager(object):
+class TutorialManager(object):
     def __init__(self, env):
         """
         """
@@ -19,39 +19,41 @@ class SuiteManager(object):
         # Load the user's progress and fill out the suite_overview template.
         user_progress = ProgressTracker(cherrypy.request.login)
 
-        template = self.env.get_template('suite_overview.html')
-        return template.render(username=cherrypy.request.login, suite_data=user_progress.load_user_progress())
+        template = self.env.get_template('tutorial_overview.html')
+        return template.render(username=cherrypy.request.login, tutorial_data=user_progress.load_user_progress())
 
     @cherrypy.expose
-    def take_suite(self, suite_num=0):
+    def take_tutorial(self, tutorial_num=0):
         """
-        Loads the suite object and directs the user to an editing session.
+        Loads the tutorial object and directs the user to an editing session.
         """
-        test_suite = TestSuiteFactory().get_suite(suite_num)
+        tutorial = TutorialFactory().get_tutorial(tutorial_num)
 
         user_progress = ProgressTracker(cherrypy.request.login)
-        if user_progress.has_progress(suite_num):
-            user_progress.update(test_suite)
+        if user_progress.has_progress(tutorial_num):
+            user_progress.update(tutorial)
 
-        template = self.env.get_template('suite.html')
-        return template.render(testsuite = test_suite, test_id = suite_num)
+        template = self.env.get_template('tutorial.html')
+        return template.render(tutorial = tutorial, tutorial_id = tutorial_num)
 
     @cherrypy.expose
-    def save(self, suite_id, user_code):
-        print(f"{cherrypy.request.login} saved their progress for suite: {suite_id}")
+    def save(self, tutorial_id, user_code):
+        """
+        Saves the contents of user_code to the user's folder.
+        """
         user_progress = ProgressTracker(cherrypy.request.login)
-        user_progress.save_progress(suite_id, user_code)
+        user_progress.save_progress(tutorial_id, user_code)
 
     @cherrypy.expose
-    def delete(self, suite_id):
+    def delete(self, tutorial_id):
         """
         """
         user_progress = ProgressTracker(cherrypy.request.login)
-        user_progress.delete_progress(suite_id)
+        user_progress.delete_progress(tutorial_id)
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
-    def evaluate(self, suite_id, user_code):
+    def evaluate(self, tutorial_id, user_code):
         """
         Evaluates the usercode and responds with teh pass/fail data as JSON.
         """
